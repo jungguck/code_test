96c6d1682f2f9923a3ff2c4a6cd97d0e3f233496
Tuna_raccon_jk <142872340+jungguck@users.noreply.github.com>
2026-03-15 20:39:57 +0900

    [Bronze III] Title: 최댓값, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/2562. 최댓값/최댓값.py
@@ -0,0 +1,12 @@
+max_value = 0
+max_index = 0
+
+for i in range(9):
+    num = int(input())
+    
+    if num > max_value:
+        max_value = num
+        max_index = i+1
+
+print(max_value)
+print(max_index)
